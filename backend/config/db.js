8cfe768455ba185ce6d1201645acb946273d3138
devifer12 <jhetu07@gmail.com>
2025-06-08 00:46:02 +0530

    final commit for auth

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config( { path: '../.env' } );
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/garba-ticketing-app', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- backend/firebase/test-init.js
+++
@@ -1,3 +0,0 @@
-const admin = require('./admin');
-console.log("Firebase initialized successfully!");
-process.exit(0);
