39ccb81eb3d6996318d4438a8ecce3bc564d4825
devifer12 <jhetu07@gmail.com>
2025-07-07 14:34:12 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferCommands: false, // Disable mongoose buffering
+      bufferCommands: true, // Enable mongoose buffering to prevent errors before initial connection
     });
     console.log("✅ MongoDB Connected to garba-ticketing-app database");
   } catch (err) {
